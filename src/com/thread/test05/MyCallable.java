452b99b63376771c60b76a99c636bb9f4390cf21
liping <2425158457@qq.com>
2023-03-18 17:26:17 +0800

    使用Callable和FutureTask来创建线程，获取线程运行之后的结果 1、实现Callable的call方法，来创建多线程要运行的参数对象 2、通过FutrueTask来创建多线程运行结果的管理者对象，将callable传入构造参数中 3、创建线程对象（将futureTask传入构造参数），获取线程运行的结果

---
+++ src/com/thread/test05/MyCallable.java
@@ -0,0 +1,45 @@
+package com.thread.test05;
+
+import com.thread.test04.MyThread04;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+public class MyCallable implements Callable<Integer> {
+
+    List<Integer> list = new ArrayList<Integer>();
+
+    // 使用Callable
+    public MyCallable(List list) {
+        this.list = list;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        List<Integer> curList = new ArrayList<Integer>();
+        while (true){
+            synchronized (MyThread04.class){
+                if (list.size()==0) {
+                    System.out.println(Thread.currentThread().getName() + curList);
+                    break;
+                }else {
+                    // 打乱集合
+                    Collections.shuffle(list);
+                    curList.add(list.remove(0));
+                }
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        if (curList.size()==0){
+            return null;
+        }else{
+            return Collections.max(curList);
+        }
+    }
+}
---
+++ src/com/thread/test05/Test05.java
@@ -0,0 +1,41 @@
+package com.thread.test05;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+
+public class Test05 {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ArrayList<Integer> testList = new ArrayList<>();
+        // 把数组的值传递给集合list
+        Collections.addAll(testList, 10,5,20,100,30,45,60,72);
+
+        // 创建多线程要运行的参数对象
+        MyCallable myCallable = new MyCallable(testList);
+
+        // 创建多线程运行结果的管理者对象
+        FutureTask<Integer> ft1 = new FutureTask<Integer>(myCallable) ;
+        FutureTask<Integer> ft2 = new FutureTask<Integer>(myCallable) ;
+
+        // 创建线程对象
+        Thread t1 = new Thread(ft1);
+        Thread t2 = new Thread(ft2);
+        t1.setName("线程1");
+        t2.setName("线程1");
+
+        // 启动
+        t1.start();
+        t2.start();
+
+        // 获取线程运行的结果
+        Integer max1 = ft1.get();
+        Integer max2 = ft2.get();
+
+        System.out.println(max1);
+        System.out.println(max2);
+
+    }
+}
