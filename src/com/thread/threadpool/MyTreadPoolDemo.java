37de6174418607e90606b93548f2d055694f775a
liping <2425158457@qq.com>
2023-03-18 18:09:14 +0800

    自定义线程池     ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor     (核心线程数、最大线程数量、空闲线程最大存活时间、任务队列、创建线程工厂、任务的拒绝策略)
        核心线程数：不能小于0
        最大线程数量：不能小于0，最大数量>=核心线程数量
        空闲线程最大存活时间：不能小于0
        时间单位：用TimeUnit指定
        任务队列：不能为null
        创建线程工厂：不能为null
        任务的拒绝策略：不能为null

--- src/com/thread/threadpool/MyTreadPoolDemo.java
+++ src/com/thread/threadpool/MyTreadPoolDemo.java
@@ -1,7 +1,6 @@
 package com.thread.threadpool;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class MyTreadPoolDemo {
     public static void main(String[] args) {
@@ -11,18 +10,57 @@ public class MyTreadPoolDemo {
         2、Executors.newFixedThreadPool(int max) 获取一个有上限的线程池，上限是max的值
         注意：线程池需要手动销毁，不会自动销毁
          */
-        ExecutorService cachedThreadPool =  Executors.newCachedThreadPool();
-        ExecutorService fixedThreadPool =  Executors.newFixedThreadPool(1);
+//        ExecutorService cachedThreadPool =  Executors.newCachedThreadPool();
+//        ExecutorService fixedThreadPool =  Executors.newFixedThreadPool(1);
+//
+//        // 提交任务到线程
+//        fixedThreadPool.submit(new MyPoolRun());
+//        fixedThreadPool.submit(new MyPoolRun());
+//        fixedThreadPool.submit(new MyPoolRun());
+//        fixedThreadPool.submit(new MyPoolRun());
+//        fixedThreadPool.submit(new MyPoolRun());
+//
+//        // 线程池不会自动销毁，需要手动销毁，销毁线程池
+//        cachedThreadPool.shutdown();
+//        fixedThreadPool.shutdown();
 
-        // 提交任务到线程
-        fixedThreadPool.submit(new MyPoolRun());
-        fixedThreadPool.submit(new MyPoolRun());
-        fixedThreadPool.submit(new MyPoolRun());
-        fixedThreadPool.submit(new MyPoolRun());
-        fixedThreadPool.submit(new MyPoolRun());
+        /*
+        自定义线程池
+            ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor
+            (核心线程数、最大线程数量、空闲线程最大存活时间、任务队列、创建线程工厂、任务的拒绝策略)
 
-        // 线程池不会自动销毁，需要手动销毁，销毁线程池
-        cachedThreadPool.shutdown();
-        fixedThreadPool.shutdown();
+            核心线程数：不能小于0
+            最大线程数量：不能小于0，最大数量>=核心线程数量
+            空闲线程最大存活时间：不能小于0
+            时间单位：用TimeUnit指定
+            任务队列：不能为null
+            创建线程工厂：不能为null
+            任务的拒绝策略：不能为null
+         */
+
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+                3, // 核心线程数量
+                5,// 最大线程数
+                100,// 空闲线程最大存活时间
+                TimeUnit.SECONDS,// 时间单位
+                new ArrayBlockingQueue(3),// 任务队列
+                Executors.defaultThreadFactory(),// 创建线程工厂
+                new ThreadPoolExecutor.AbortPolicy()// 任务的拒绝策略
+        );
+        // 提交任务到线程池
+        threadPoolExecutor.submit(new MyPoolRun());
+        threadPoolExecutor.submit(new MyPoolRun());
+        threadPoolExecutor.submit(new MyPoolRun());
+        threadPoolExecutor.submit(new MyPoolRun());
+        threadPoolExecutor.submit(new MyPoolRun());
+        threadPoolExecutor.submit(new MyPoolRun());
+        threadPoolExecutor.submit(new MyPoolRun());
+        threadPoolExecutor.submit(new MyPoolRun());
+        threadPoolExecutor.submit(new MyPoolRun());
+        threadPoolExecutor.submit(new MyPoolRun());
+        // 销毁
+        threadPoolExecutor.shutdown();
+
+
     }
 }
